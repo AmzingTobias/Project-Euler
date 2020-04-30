e802e929a0bbf25e625c5739e74daccfdd4fcf4f
AmzingTobias <40802800+AmzingTobuscus@users.noreply.github.com>
2020-04-30 15:23:40 +0100

    Problem 1 complete

---
+++ Problem 1/main.py
@@ -0,0 +1,12 @@
+total = 0
+# All numbers bellow the target
+for factor in range(1000):
+    # Checks if the factor being checked is divisble
+    # by 3 or 5 with no remainders, hence
+    # checking for the remainders of the division
+    # to equal 0
+    if factor % 3 == 0 or factor % 5 == 0:
+        # Will add the total to the factor
+        total += factor
+print(total)
+# Answer = 233168
