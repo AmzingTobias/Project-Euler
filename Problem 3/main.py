05146f916afd9fe700cb3e0411d7a61b21fc9474
AmzingTobias <40802800+AmzingTobuscus@users.noreply.github.com>
2020-04-30 16:38:35 +0100

    Added Problem 3

---
+++ Problem 3/main.py
@@ -0,0 +1,16 @@
+target = 600851475143
+lpf = 2
+# Loops while the target is greater than 2
+while target > lpf:
+    # Checks if the highest factor is a factor
+    # of the target
+    if target % lpf == 0:
+        # Finds the division of that number
+        target = target / lpf
+        # Resets the largest factor
+        lpf = 2
+    else:
+        # Adds one to the factor otherwise to then check        
+        lpf += 1
+print(lpf)
+# Answer: 6857
